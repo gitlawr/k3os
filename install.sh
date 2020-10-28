7597389c86e33ab8faa4a5c2fc0298a8be6c3342
gitlawr <lawrleegle@gmail.com>
2020-10-28 19:27:46 +0800

    bump partition size

--- install.sh
+++ install.sh
@@ -81,11 +81,11 @@ do_format()
         BOOT_NUM=1
         STATE_NUM=2
         parted -s ${DEVICE} mkpart primary fat32 0% 50MB
-        parted -s ${DEVICE} mkpart primary ext4 50MB 4196MB
+        parted -s ${DEVICE} mkpart primary ext4 50MB 6196MB
     else
         BOOT_NUM=
         STATE_NUM=1
-        parted -s ${DEVICE} mkpart primary ext4 0% 4096MB
+        parted -s ${DEVICE} mkpart primary ext4 0% 6144MB
     fi
     parted -s ${DEVICE} set 1 ${BOOTFLAG} on
     partprobe ${DEVICE} 2>/dev/null || true
